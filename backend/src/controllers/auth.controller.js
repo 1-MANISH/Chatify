c0de06fd729244e8a28e49974f6efb28889fe245
manish_patidar <shanimpatidar@gmail.com>
2025-12-22 13:47:18 +0530

    updated with auth:routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,11 +67,69 @@ export const signup = async(req,res) => {
        
 }
 export const login = async(req,res) => {
-        res.send('Login Route');
+       try {
+
+        const {email,password} = req.body
+
+        if(!email || !password) {
+                return res.status(400).json({message: 'All fields are required'});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+                        return res.status(400).json({ message: 'Invalid email format' });
+        }
+
+        const user = await User.findOne({email})
+
+        if(!user){
+                return res.status(400).json({message: 'Invalid credentials'});
+        }
+
+        const passwordCorrect = await bcrypt.compare(password,user.password);
+
+        if(!passwordCorrect){
+                return res.status(400).json({message: 'Invalid credentials'});
+        }
+
+        generateToken(user._id,res) // cookie set done here
+
+        res.status(200).json({
+                message: 'Login successful',
+                user:{
+                        _id:user._id,
+                        fullName:user.fullName,
+                        email:user.email,
+                        profilePicture:user.profilePicture,
+                }
+        })
+
+       } catch (error) {
+                console.error('Error in login:', error.message);
+                return res.status(500).json({ message: 'Internal Server Error' });
+       }
 }
-export const logout = async(req,res) => {
-        res.send('Logout Route');
+export const logout = async(_req,res) => {
+        try {
+
+                res.cookie(
+                        'chatify_jwt_token',
+                        "",
+                        {
+                                maxAge:0,
+                        }
+                )
+
+                res.status(200).json({
+                        message: 'Logout successful',
+                })
+                
+        } catch (error) {
+                 console.error('Error in logout:', error.message);
+                return res.status(500).json({ message: 'Internal Server Error' });
+        }
 }
+
 
 /*
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,10 +5,8 @@ const router = Router();
 
 
 router.post('/signup',signup)
-
 router.post('/login',login)
-
-router.get('/logout',logout)
+router.post('/logout',logout)
 
 
 export default router;
